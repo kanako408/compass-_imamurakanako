1b02056078266b7e3afcdbea245878c7f12224db
new_workspece-admin <kon.3.25@gmail.com>
2025-02-16 16:19:34 +0900

    環境構築

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '37d25fccdd0f53a71659ec8e2f65cb3bc8b05a51',
+        'reference' => '5a9f3cb0d630490b51bba29153381659a2ffec2f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -367,7 +367,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '37d25fccdd0f53a71659ec8e2f65cb3bc8b05a51',
+            'reference' => '5a9f3cb0d630490b51bba29153381659a2ffec2f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -661,8 +661,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
