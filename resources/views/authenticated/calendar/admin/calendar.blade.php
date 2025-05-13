dcd54919c3ccd5eab8338639c761d26214e3e002
new_workspece-admin <kon.3.25@gmail.com>
2025-05-13 23:22:02 +0900

    全体レイアウト修正⓵

--- resources/views/authenticated/calendar/admin/calendar.blade.php
+++ resources/views/authenticated/calendar/admin/calendar.blade.php
@@ -1,8 +1,9 @@
 <x-sidebar>
-<div class="w-75 m-auto">
-  <div class="w-100">
-    <p>{{ $calendar->getTitle() }}</p>
-    <p>{!! $calendar->render() !!}</p>
+  <div class="w-75 m-auto">
+    <div class="w-100">
+
+      <p>{{ $calendar->getTitle() }}</p>
+      <p>{!! $calendar->render() !!}</p>
+    </div>
   </div>
-</div>
 </x-sidebar>
